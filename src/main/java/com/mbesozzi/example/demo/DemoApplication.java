94c6764b209c9f78351fc6103df1ad42ff7ba8b5
Vincent Wong <vincent.ch.wong@gmail.com>
2023-03-14 09:37:41 +0800

    docs: update comment

--- src/main/java/com/mbesozzi/example/demo/DemoApplication.java
+++ src/main/java/com/mbesozzi/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	// add comment for test
+	// update comment for test
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
